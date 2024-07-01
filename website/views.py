d5afcac360e1cf0ef19f69d4ec970eb28c250cbd
PhanQuan10423098 <10423098@student.vgu.edu.vn>
2024-07-01 15:55:17 +0700

    Bug fix

--- website/views.py
+++ website/views.py
@@ -6,7 +6,7 @@ import json
 
 views = Blueprint('views', __name__)
 
-@views.route('/home')
+@views.route('/')
 @login_required
 def home():
     return render_template("home.html", user=current_user)
